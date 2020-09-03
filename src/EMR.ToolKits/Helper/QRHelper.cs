78298bc0c9a0245d173088fc768f4897dcc258a5
WillJun <wu567jun123@163.com>
2020-09-03 23:50:57 +0800

    参数介绍

--- src/EMR.ToolKits/Helper/QRHelper.cs
+++ src/EMR.ToolKits/Helper/QRHelper.cs
@@ -194,7 +194,7 @@ namespace EMR.ToolKits.Helper
 
 
 
-        public static Bitmap GetQRCodeWithLogo(string plainText, int pixel, string logoPath)
+        public static Bitmap GetQRCodeWithLogo(string plainText, int pixel = 30, string logoPath = "")
         {
 
             if (string.IsNullOrWhiteSpace(logoPath))
@@ -204,18 +204,27 @@ namespace EMR.ToolKits.Helper
 
             var generator = new QRCodeGenerator();
             var qrCodeData = generator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.H);
-            var qrCode = new QRCoder.QRCode(qrCodeData);
-
+            var qrCode = new QRCode(qrCodeData);
+            #region 参数介绍
+            //GetGraphic方法参数介绍
+            //pixelsPerModule //生成二维码图片的像素大小 ，我这里设置的是5
+            //darkColor       //暗色   一般设置为Color.Black 黑色
+            //lightColor      //亮色   一般设置为Color.White  白色
+            //icon             //二维码 水印图标 例如：Bitmap icon = new Bitmap(context.Server.MapPath("~/images/zs.png")); 默认为NULL ，加上这个二维码中间会显示一个图标
+            //iconSizePercent  //水印图标的大小比例 ，可根据自己的喜好设置
+            //iconBorderWidth  // 水印图标的边框
+            //drawQuietZones   //静止区，位于二维码某一边的空白边界,用来阻止读者获取与正在浏览的二维码无关的信息 即是否绘画二维码的空白边框区域 默认为true
+            #endregion
             var bitmap = qrCode.GetGraphic(pixel, Color.Black, Color.White, (Bitmap)Image.FromFile(logoPath), 15, 8);
 
             return bitmap;
         }
 
-        public static Bitmap GetQRCode(string plainText, int pixel)
+        public static Bitmap GetQRCode(string plainText, int pixel = 30)
         {
             var generator = new QRCodeGenerator();
-            var qrCodeData = generator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.Q);
-            var qrCode = new QRCoder.QRCode(qrCodeData);
+            var qrCodeData = generator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.H);
+            var qrCode = new QRCode(qrCodeData);
 
             var bitmap = qrCode.GetGraphic(pixel);
 
